4ef65657d08d90aaa96b4114dbb211c02c3adf27
Hao Wang <wanghao@london.csl.utoronto.ca>
2016-07-04 19:06:01 -0400

    fix bugs in monitor_parser.py

--- monitor_parser.py
+++ monitor_parser.py
@@ -1,8 +1,6 @@
 #!/bin/python
 __author__  = "www.haow.ca"
 
-# requirements: ansible
-
 import os, sys
 import re
 import signal
@@ -10,9 +8,11 @@ from sys import stderr
 import subprocess
 from argparse import ArgumentParser
 import time, datetime
+import os.path as path
+import csv
 
-MASTER=""
-WORKERS=[]
+WORKERS = []
+MASTER = ''
 
 def parse_args():
 	parser = ArgumentParser(usage="monitor_parser.py [options]")
@@ -34,30 +34,23 @@ def to_epoch(date_str):
 
 def parse_hosts(opts):
 	print "### Parsing hosts ###"
+	global MASTER
 	in_fd = open(opts.hosts)
-	flag_master = -1
-	flag_worker = 999
 	for i, line in enumerate(in_fd):
-		if line is None:
-			break
-		elif "master" in line:
-			flag_master = i
-		elif i == flag_master+1:
+		if i == 1:
 			MASTER = line.split(" ")[0]
-		elif "worker" in line:
-			flag_worker = i
-		elif i > flag_worker:
+		elif i > 3:
 			WORKERS.append(line.split(" ")[0])
-	
 
-def parse_log(opts):
+
+def parse_log(exp_path, opts):
 	print "### Parsing log ###"
-	in_fd = open(opts.path+"/spark.log")
-	out_fds = {} 
+	in_fd = open(exp_path+"/spark.log")
+	out_fds = {}
 	for worker in WORKERS:
 		out_fds[worker.strip()] = open(
-				opts.path+"/task-%s.log"%worker.split(".")[0], 'w')
-  
+				exp_path+"/task-%s.log"%worker.split(".")[0], 'w')
+
 	visited = False
 	for line in in_fd.readlines():
 		if "Finished task" in line:
@@ -68,7 +61,7 @@ def parse_log(opts):
 			stage_id = line_splitted[11]
 			t_cost = int(line_splitted[15])
 			host = line_splitted[18]
-      
+
 			start_time = to_epoch(date+" "+time) - t_cost/1000
 
 			if not visited:
@@ -76,18 +69,21 @@ def parse_log(opts):
 				start_time = 0
 				visited = True
 			else:
-			  start_time = start_time - base_time
+				start_time = start_time - base_time
+
+			if 'ip' in host:
+				host = WORKERS[0]
 
 			finish_time = start_time + t_cost/1000
 			out_fds[host].write("%d\t%d\t%s\t%s\n"\
-					% (start_time, finish_time, task_id, stage_id)) 
+					% (start_time, finish_time, task_id, stage_id))
 
 	in_fd.close()
 	for fd in out_fds.values():
 		fd.close()
 
 
-def parse_disk(in_fd, out_fd): 
+def parse_disk(in_fd, out_fd):
 	print "### Parsing disk ###"
 	for line in in_fd.readlines():
 		disk_util = 0
@@ -106,7 +102,7 @@ def parse_jvm(in_fd, out_fd):
 	# exe-mem exe-cpu exe-gc data-mem data-cpu data-gc name-mem name-cpu name-gc
 	pt_executor = "ExecutorBackend"
 	pt_datanode = "tanode.DataNode"
-	pt_namenode = "tanode.NameNode"
+	# pt_namenode = "tanode.NameNode"
 	ext_id = dn_id = None
 
 	#header = ("exe-mem\t exe-cpu\t exe-gc\t"
@@ -119,27 +115,27 @@ def parse_jvm(in_fd, out_fd):
 	for line in in_fd.readlines():
 		if pt_executor in line:
 			ext_id = line.split()[0].strip()
-			exe_mem = line.split()[2].strip()	
-			exe_cpu = line.split()[6].strip()	
-			exe_gc = line.split()[7].strip()	
+			exe_mem = line.split()[2].strip()
+			exe_cpu = line.split()[6].strip()
+			exe_gc = line.split()[7].strip()
 			outline[0] = exe_mem[:-1]
 			outline[1] = exe_cpu[:-1]
 			outline[2] = exe_gc[:-1]
 		elif pt_datanode in line:
 			dn_id = line.split()[0].strip()
-			dn_mem = line.split()[2].strip()	
-			dn_cpu = line.split()[6].strip()	
-			dn_gc = line.split()[7].strip()	
+			dn_mem = line.split()[2].strip()
+			dn_cpu = line.split()[6].strip()
+			dn_gc = line.split()[7].strip()
 			outline[3] = dn_mem[:-1]
 			outline[4] = dn_cpu[:-1]
 			outline[5] = dn_gc[:-1]
-		elif pt_namenode in line:
-			nd_mem = line.split()[2].strip()	
-			nd_cpu = line.split()[6].strip()	
-			nd_gc = line.split()[7].strip()	
-			outline[6] = nd_mem[:-1]
-			outline[7] = nd_cpu[:-1]
-			outline[8] = nd_gc[:-1]
+		# elif pt_namenode in line:
+		# 	nd_mem = line.split()[2].strip()
+		# 	nd_cpu = line.split()[6].strip()
+		# 	nd_gc = line.split()[7].strip()
+		# 	outline[6] = nd_mem[:-1]
+		# 	outline[7] = nd_cpu[:-1]
+		# 	outline[8] = nd_gc[:-1]
 		elif not line.strip():
 			out_str = "\t".join(map(str, outline))
 			if out_str.strip():
@@ -179,15 +175,15 @@ def parse_net(in_fd, out_fd, ext_id, dn_id):
 				outline = [0]*4
 			else:
 				continue
-		
 
-def parse(opts):
+
+def parse(exp_path, opts):
 	# walk through the directory
 	# suppose the input directory is /monitor/146359****
 	parse_hosts(opts)
-	parse_log(opts) # read in spark.log and output task start/finish time
+	parse_log(exp_path, opts) # read in spark.log and output task start/finish time
 
-	for d, sub_d, f_list in os.walk(opts.path):
+	for d, sub_d, f_list in os.walk(exp_path):
 		if f_list:
 			# iterate in the order of disk, jvmtop, net
 			f_list.sort()
@@ -218,11 +214,19 @@ def parse(opts):
 				out_fd.close()
 
 
+def merge():
+	pass
+
+
 def main():
 	opts = parse_args()
-	#parse_hosts(opts)
+	for d in os.listdir(opts.path):
+		if os.path.isdir(os.path.join(opts.path, d)):
+			print "#### Parsing " + d + " ####"
+			parse(path.abspath(path.join(opts.path, d)), opts)
+
 	#parse_log(opts)
-	parse(opts)
+	# parse(opts)
 
 
 if __name__ == "__main__":
