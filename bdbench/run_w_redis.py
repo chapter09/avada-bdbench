748d32d5b3dd841b7a417e6714dd992d272e42b2
hao <wh.sjtu@gmail.com>
2016-07-21 02:44:31 -0400

    update run with redis

--- bdbench/run_w_redis.py
+++ bdbench/run_w_redis.py
@@ -14,7 +14,7 @@ import time
 import datetime
 
 BASE_DIR = "~/monitor/"
-MASTER = "ec2-54-224-95-250.compute-1.amazonaws.com"
+MASTER = ""
 
 
 def parse_args():
@@ -34,11 +34,10 @@ def parse_args():
                         help="Address of Sparl master")
     opts = parser.parse_args()
 
-    if opts.hosts is None:
+    if opts.hosts is None or opts.spark_master is None:
         print opts.print_help()
-
-    if opts.spark_master:
-        MASTER = opts.spark_master 
+        exit(0)
+    MASTER = opts.spark_master 
 
     return opts
 
@@ -51,59 +50,53 @@ def ansible_exe(host, command):
     # os.setsid creates a new process session
     # all descendant processes will be added into this session
     # then we could kill them all by process group id
-    return subprocess.Popen("ansible %s -m shell -a  \"%s async=45 poll=5\"" % (
+    p = subprocess.Popen("ansible %s -m shell -a  \"%s async=45 poll=5\"" % (
         host, command), shell=True, preexec_fn=os.setsid)
+    p.wait()
 
 
 def pre_run(opts, prefix):
     try:
         # create local monitor directory
-        print "####Create local monitor directory####"
+        print "----Create local monitor directory----"
         execute("mkdir -p " + BASE_DIR + prefix)
-        # create remote monitor directory
-        print "####Create remote monitor directory####"
-        p = ansible_exe(opts.node, "mkdir -p %s/{{inventory_hostname}}" % (
-           BASE_DIR + prefix))
-        p.wait()
 
-        print "####Launch Redis Server####"
-        p = ansible_exe(opts.node, "./monitor_daemon.py &")
-        p.wait()
+        # create remote monitor directory
+        print "----Create remote monitor directory----"
+        ansible_exe(opts.node, "mkdir -p %s/{{inventory_hostname}}" % (
+           BASE_DIR + prefix))
+
+        print "----Launch monitoring daemon----"
+        ansible_exe(opts.node, "./monitor_daemon.py &")
 
     except subprocess.CalledProcessError as e:
         return e.returncode
 
 
-def post_run(opts, prefix):
-    # parse data from redis-server
+def post_run(opts, prefix, start_time, end_time):
+    # kill monitor daemon
+    print "----Kill monitor daemon process----"
+    ansible_exe(opts.node, "pkill monitor_daemon.py")
 
-    # create a tar package to include results of this time
-    print "####Build packge of remote results####"
-    cmd = "tar -cf %s/%s/{{inventory_hostname}}.tar " % (BASE_DIR, prefix) \
-        + "-C %s/ {{inventory_hostname}}" % (BASE_DIR + prefix)
-    p = ansible_exe(opts.node, cmd)
-    p.wait()
+    # parse data from redis-server
+    cmd = "./redis_to_file.py -s %s -e %s -d {{ inventory_hostname }} -q % s" \
+        % (start_time, end_time, opts.query_num)
+    ansible_exe(opts.node, cmd)
 
     # remote fetch
-    print "####Fetch remote reulst packges####"
-    cmd = "src=%s%s/{{inventory_hostname}}.tar dest=%s%s/ " % (
-        BASE_DIR, prefix, BASE_DIR, prefix) + "flat=yes validate_checksum=no"
-    p = subprocess.Popen("ansible %s -m fetch -a \"%s\"" % (
-        opts.node, cmd), shell=True)
-    p.wait()
+    print "----Fetch remote reulst packges----"
+    cmd = "scp ~/monitor/%s-* {{ groups['master'][0] }}:%s%s" \
+        % (start_time, BASE_DIR, prefix)
+    ansible_exe(opts.node, cmd)
 
     # local tar package extract
-    print "####Extract reulst packges####"
+    print "----Extract reulst packges----"
     cmd = "cat %s/*.tar | tar -xf - -i -C %s/" % (
         BASE_DIR + prefix, BASE_DIR + prefix)
     execute(cmd)
 
-    # copy spark.log to direcotry
-    print "####Move spark.log to monitoring directory####"
-    execute("mv ~/spark-1.6.1/logs/spark.log " + BASE_DIR + prefix + "/")
-
     # delete all tar packages
-    print "####Remote reulst tar packges####"
+    print "----Remote reulst tar packges----"
     cmd = "rm %s/*.tar" % (BASE_DIR + prefix)
     execute(cmd)
 
@@ -114,18 +107,19 @@ def run(opts):
 
     start_time = prefix
     cmd = "python avada-ML/monitor-daemon.py"
-    p = ansible_exe(opts.node, cmd)
+    ansible_exe(opts.node, cmd)
     
-    print "####Run Spark SQL query %s####" % opts.query_num
-    run_query = ("python run_query.py --spark-master spark://%s:7077" 
+    print "----Run Spark SQL query %s----" % opts.query_num
+    run_query = ("python run_query.py"
+                 " --spark-master spark://%s:7077" 
                  " -q %s --num-trials %s") % (
-                         MASTER, opts.query_num, opts.trial_num)
+                     MASTER, opts.query_num, opts.trial_num)
 
     query_p = subprocess.Popen(run_query, shell=True)
     query_p.wait()
     
-    print "####Finish Spark SQL query####"
-    end_time = time.time()
+    print "----Finish Spark SQL query----"
+    end_time = str(time.time()).split(".")[0]
 
     for p in monitor_proc:
         print "Killing: \"%s\"" % run_monitor[monitor_proc.index(p)]
